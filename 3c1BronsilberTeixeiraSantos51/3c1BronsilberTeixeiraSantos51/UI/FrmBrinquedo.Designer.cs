748bf77be8581a9bb59fcae017118f97d3f99a82
bron.teixeira.s@gmail.com <79947948+bron9782@users.noreply.github.com>
2021-11-19 18:13:16 -0300

    Parte de brinquedos

--- 3c1BronsilberTeixeiraSantos51/3c1BronsilberTeixeiraSantos51/UI/FrmBrinquedo.Designer.cs
+++ 3c1BronsilberTeixeiraSantos51/3c1BronsilberTeixeiraSantos51/UI/FrmBrinquedo.Designer.cs
@@ -29,12 +29,161 @@ namespace _3c1BronsilberTeixeiraSantos51.UI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.MaskedTextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.MaskedTextBox();
+            this.dgvProdutos = new System.Windows.Forms.DataGridView();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btn_Cadastrar = new System.Windows.Forms.Button();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.lblPreco = new System.Windows.Forms.Label();
+            this.lbl_codigo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(78, 132);
+            this.txtMarca.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(137, 20);
+            this.txtMarca.TabIndex = 71;
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(75, 117);
+            this.lblMarca.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(37, 13);
+            this.lblMarca.TabIndex = 70;
+            this.lblMarca.Text = "Marca";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(78, 36);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(137, 20);
+            this.txtNome.TabIndex = 69;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(75, 20);
+            this.lblNome.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 68;
+            this.lblNome.Text = "Nome";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(78, 87);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 67;
+            // 
+            // dgvProdutos
+            // 
+            this.dgvProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProdutos.Location = new System.Drawing.Point(344, 11);
+            this.dgvProdutos.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvProdutos.Name = "dgvProdutos";
+            this.dgvProdutos.RowHeadersWidth = 62;
+            this.dgvProdutos.RowTemplate.Height = 28;
+            this.dgvProdutos.Size = new System.Drawing.Size(445, 413);
+            this.dgvProdutos.TabIndex = 66;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.Color.Maroon;
+            this.btnExcluir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnExcluir.Location = new System.Drawing.Point(78, 351);
+            this.btnExcluir.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(163, 50);
+            this.btnExcluir.TabIndex = 65;
+            this.btnExcluir.Text = "EXCLUIR";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            // 
+            // btn_Cadastrar
+            // 
+            this.btn_Cadastrar.BackColor = System.Drawing.Color.Green;
+            this.btn_Cadastrar.ForeColor = System.Drawing.SystemColors.Menu;
+            this.btn_Cadastrar.Location = new System.Drawing.Point(78, 273);
+            this.btn_Cadastrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Cadastrar.Name = "btn_Cadastrar";
+            this.btn_Cadastrar.Size = new System.Drawing.Size(163, 52);
+            this.btn_Cadastrar.TabIndex = 64;
+            this.btn_Cadastrar.Text = "CADASTRAR";
+            this.btn_Cadastrar.UseVisualStyleBackColor = false;
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(78, 186);
+            this.txtPreco.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(87, 20);
+            this.txtPreco.TabIndex = 62;
+            // 
+            // lblPreco
+            // 
+            this.lblPreco.AutoSize = true;
+            this.lblPreco.Location = new System.Drawing.Point(75, 171);
+            this.lblPreco.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPreco.Name = "lblPreco";
+            this.lblPreco.Size = new System.Drawing.Size(35, 13);
+            this.lblPreco.TabIndex = 61;
+            this.lblPreco.Text = "Preço";
+            // 
+            // lbl_codigo
+            // 
+            this.lbl_codigo.AutoSize = true;
+            this.lbl_codigo.Location = new System.Drawing.Point(75, 71);
+            this.lbl_codigo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_codigo.Name = "lbl_codigo";
+            this.lbl_codigo.Size = new System.Drawing.Size(40, 13);
+            this.lbl_codigo.TabIndex = 59;
+            this.lbl_codigo.Text = "Código";
+            // 
+            // FrmBrinquedo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMarca);
+            this.Controls.Add(this.lblMarca);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.dgvProdutos);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btn_Cadastrar);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.lblPreco);
+            this.Controls.Add(this.lbl_codigo);
+            this.Name = "FrmBrinquedo";
             this.Text = "FrmBrinquedo";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.MaskedTextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.MaskedTextBox txtCodigo;
+        private System.Windows.Forms.DataGridView dgvProdutos;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btn_Cadastrar;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.Label lblPreco;
+        private System.Windows.Forms.Label lbl_codigo;
     }
 }
