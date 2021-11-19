5908cfc441848134a41e3bf6c10a74fb5009cb3b
bron.teixeira.s@gmail.com <79947948+bron9782@users.noreply.github.com>
2021-11-19 17:42:01 -0300

    design login

--- 3c1BronsilberTeixeiraSantos51/3c1BronsilberTeixeiraSantos51/UI/frmUsuario.Designer.cs
+++ 3c1BronsilberTeixeiraSantos51/3c1BronsilberTeixeiraSantos51/UI/frmUsuario.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace _3c1BronsilberTeixeiraSantos51
 {
-    partial class Form1
+    partial class frmUsuario
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -29,13 +29,86 @@ namespace _3c1BronsilberTeixeiraSantos51
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lbl_Cpf = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.BackColor = System.Drawing.Color.Purple;
+            this.btnEntrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnEntrar.Location = new System.Drawing.Point(333, 267);
+            this.btnEntrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(137, 41);
+            this.btnEntrar.TabIndex = 62;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = false;
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(333, 145);
+            this.txtemail.Margin = new System.Windows.Forms.Padding(2);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(137, 20);
+            this.txtemail.TabIndex = 60;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(330, 120);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 59;
+            this.lblEmail.Text = "E-mail";
+            // 
+            // lbl_Cpf
+            // 
+            this.lbl_Cpf.AutoSize = true;
+            this.lbl_Cpf.Location = new System.Drawing.Point(330, 190);
+            this.lbl_Cpf.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Cpf.Name = "lbl_Cpf";
+            this.lbl_Cpf.Size = new System.Drawing.Size(22, 13);
+            this.lbl_Cpf.TabIndex = 58;
+            this.lbl_Cpf.Text = "cpf";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(333, 206);
+            this.maskedTextBox1.Mask = "999.999.999-99";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(137, 20);
+            this.maskedTextBox1.TabIndex = 63;
+            // 
+            // frmUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lbl_Cpf);
+            this.Name = "frmUsuario";
+            this.Text = "frmUsuario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lbl_Cpf;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
     }
 }
 
--- 3c1BronsilberTeixeiraSantos51/3c1BronsilberTeixeiraSantos51/UI/frmUsuario.cs
+++ 3c1BronsilberTeixeiraSantos51/3c1BronsilberTeixeiraSantos51/UI/frmUsuario.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace _3c1BronsilberTeixeiraSantos51
 {
-    public partial class Form1 : Form
+    public partial class frmUsuario : Form
     {
-        public Form1()
+        public frmUsuario()
         {
             InitializeComponent();
         }
