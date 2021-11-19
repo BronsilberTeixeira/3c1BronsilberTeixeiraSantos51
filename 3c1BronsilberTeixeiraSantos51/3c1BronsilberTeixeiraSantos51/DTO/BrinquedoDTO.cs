ffa271fe7a8a1f272da3f56bcf0f156d9ecc729a
bron.teixeira.s@gmail.com <79947948+bron9782@users.noreply.github.com>
2021-11-19 18:04:44 -0300

    Dto de brinquedo atualizado

--- 3c1BronsilberTeixeiraSantos51/3c1BronsilberTeixeiraSantos51/DTO/BrinquedoDTO.cs
+++ 3c1BronsilberTeixeiraSantos51/3c1BronsilberTeixeiraSantos51/DTO/BrinquedoDTO.cs
@@ -11,11 +11,11 @@ namespace _3c1BronsilberTeixeiraSantos51.DTO
         private int _codigo;
         private string _nome;
         private double _preco;
-        private string _fornecedor;
+        private string _marca;
         private string _categoria;
 
         public string nome { get => _nome; set => _nome = value; }
-        public string fornecedor { get => _fornecedor; set => _fornecedor = value; }
+        public string marca { get => _marca; set => _marca = value; }
         public int codigo { get => _codigo; set => _codigo = value; }
         public string categoria { get => _categoria; set => _categoria = value; }
         public double preco { get => _preco; set => _preco = value; }
