fd3e82d11f7c7d1eed907744da215d3979c2e85c
bron.teixeira.s@gmail.com <79947948+bron9782@users.noreply.github.com>
2021-11-19 18:09:30 -0300

    brinquedo DTO

--- 3c1BronsilberTeixeiraSantos51/3c1BronsilberTeixeiraSantos51/BLL/BrinquedoDLL.cs
+++ 3c1BronsilberTeixeiraSantos51/3c1BronsilberTeixeiraSantos51/BLL/BrinquedoDLL.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using _3c1BronsilberTeixeiraSantos51.DAL;
+using _3c1BronsilberTeixeiraSantos51.DTO;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,23 @@ namespace _3c1BronsilberTeixeiraSantos51.BLL
 {
     class BrinquedoDLL
     {
+        Conexao conexao = new Conexao();
+        string tabela = "brinquedos";
+
+        public void Inserir(BrinquedoDTO Brinquedodto)
+        {
+            string inserir = $"insert into {tabela} values({Brinquedodto.nome}, '{Brinquedodto.codigo}','{Brinquedodto.preco}','{Brinquedodto.categoria}', '{Brinquedodto.marca}')";
+            conexao.ExecutarComando(inserir);
+        }
+        public DataTable Listar()
+        {
+            string listar = $"select * from {tabela} order by codigo";
+            return conexao.ExecutarConsulta(listar);
+        }
+        public void excluir(BrinquedoDTO BrinquedodtO)
+        {
+            string excluir = $"delete from {tabela} where codigo = '{BrinquedodtO.codigo}';";
+            conexao.ExecutarComando(excluir);
+        }
     }
 }
