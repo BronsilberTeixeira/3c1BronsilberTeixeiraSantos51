eb6927cfa5dfc127ba1e8f8dd2c9126d850a207e
bron.teixeira.s@gmail.com <79947948+bron9782@users.noreply.github.com>
2021-11-19 17:44:34 -0300

    conexao com o Banco

--- 3c1BronsilberTeixeiraSantos51/3c1BronsilberTeixeiraSantos51/DAL/Conexao.cs
+++ 3c1BronsilberTeixeiraSantos51/3c1BronsilberTeixeiraSantos51/DAL/Conexao.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,57 @@ namespace _3c1BronsilberTeixeiraSantos51.DAL
 {
     class Conexao
     {
+        MySqlConnection conexaoBanco;
+        public void Conectar()
+        {
+            try
+            {
+                string conn = "Persist Security Info = false; " +
+                              "server = localhost; " +
+                              "database = 3c1bronsilberteixeirasantos51;" +
+                              "uid = root; pwd=";
+                conexaoBanco = new MySqlConnection(conn);
+                conexaoBanco.Open();
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("Não foi possível conectar ao banco de dados.\n" + ex.Message);
+            }
+        }
+        public DataTable ExecutarConsulta(string sql)
+        {
+            try
+            {
+                Conectar();
+                MySqlDataAdapter dados = new MySqlDataAdapter(sql, conexaoBanco);
+                DataTable dt = new DataTable();
+                dados.Fill(dt);
+                return dt;
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("Não foi possível executar a CONSULTA solicitada.\n" + ex.Message);
+            }
+            finally
+            {
+                conexaoBanco.Close();
+            }
+        }
+        public void ExecutarComando(string sql)
+        {
+            Conectar();
+            try
+            {
+                MySqlCommand comando = new MySqlCommand(sql, conexaoBanco);
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("A instrução não foi realizada.\n" + ex.Message);
+            }
+            finally
+            {
+                conexaoBanco.Close();
+            }
+        }
     }
 }
